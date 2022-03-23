6a062088f322dabefac558b0b52852113bbdf242
Amy Gilligan <agilligan27@gmail.com>
2022-03-22 20:44:40 -0400

    Update JS file with new version

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var lossMessages = ["Sorry, you lose.", "Sorry, try again.", "Better luck next t
 
 // Create variables to count wins and losses
 
-var winCount
+var winCount = " ";
 
-var lossCount
+var lossCount = " ";
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
 
@@ -17,13 +17,38 @@ var lossSection = document.getElementById('losses');
 var displayMessage = document.getElementById('message');
 
 // target all .box elements and attach a click event listener to each one using a loop
-
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
+
+var boxOne = document.getElementById("box-1");
+var boxTwo = document.getElementById("box-2");
+var boxThree = document.getElementById("box-3");
+
+var allBoxes = [boxOne, boxTwo, boxThree];
+
+
+for (let i = 0; i < allBoxes.length; i++){
+    allBoxes.addEventListener("click", boxNumber(e))
+}
+
+function boxNumber (e) {
+    var clickedBox = this.textContent;
+    return clickedBox;
+    
+}
+
+
+
+
+
+
 // convert that value to a Number and store it to a variable
+
+
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
+
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
